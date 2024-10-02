620948482649969a41c34823815b4f116b26dd89
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-02 16:41:57 +0300

    fox2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Node.js is amazing!');
+const sum = (a, b) => a + b;
+
+sum(2, 3);
+
+class Hero {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+}
+
+const h = new Hero('Mango');
